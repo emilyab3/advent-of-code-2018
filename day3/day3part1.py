02ffc4df9eaf113add41bb82756e8461423cd4dc
Emily <emilyb3333@gmail.com>
2018-12-03 22:16:18 +1000

    Cleaned up and commented Day 3

--- day3/day3part1.py
+++ day3/day3part1.py
@@ -3,7 +3,10 @@ from parse import *
 
 
 class ElfRectangle:
+    """ A class to represent a rectangle on a piece of fabric chosen by an elf"""
+
     def __init__(self, rect_id, left, top, width, height):
+        """Creates a new ElfRectangle object with the given properties"""
         self.rect_id = rect_id
         self.left = left
         self.top = top
@@ -11,8 +14,13 @@ class ElfRectangle:
         self.height = height
 
 
-# Format: #id @ left,top: widthxheight
 def decode_rectangle(encoded: str) -> ElfRectangle:
+    """
+    Decodes a string representing an ElfRectangle
+    The string should be of the format: #id @ left,top: widthxheight
+    :param encoded: the string to be decoded
+    :return: the ElfRectangle decoded from the string
+    """
     parsed = parse("#{rect_id:d} @ {left:d},{top:d}: {width:d}x{height:d}", encoded)
     rectangle = ElfRectangle(parsed["rect_id"], parsed["left"], parsed["top"],
                              parsed["width"], parsed["height"])
@@ -20,6 +28,11 @@ def decode_rectangle(encoded: str) -> ElfRectangle:
 
 
 def read_input(filename: str):
+    """
+    Reads input from the given file
+    :param filename: the file to read from
+    :return: a list of the ElfRectangles contained in the file
+    """
     rectangles = []
     with open(filename, 'r') as file:
         for line in file:
@@ -30,6 +43,13 @@ def read_input(filename: str):
 
 
 def build_grid(rectangles):
+    """
+    Builds a representation of all the ElfRectangles mapped out on a large piece
+    of fabric. Each square inch is represented by a coordinate.
+    :param rectangles: the rectangles to map out
+    :return: a mapping from each coordinate (x, y) on the fabric to the number of
+    ElfRectangles that coordinate is present in
+    """
     all_coords = {}
     for rect in rectangles:
         for x in range(rect.left, rect.left + rect.width):
@@ -44,6 +64,12 @@ def build_grid(rectangles):
 
 
 def get_overlap(rectangles):
+    """
+    Gets the number of coordinates on the piece of fabric which are overlapped by
+    multiple ElfRectangles
+    :param rectangles: the rectangles to check for overlap
+    :return: the number of coordinated which are overlapped
+    """
     all_coords = build_grid(rectangles)
 
     overlapping_coords = 0
--- day3/day3part2.py
+++ day3/day3part2.py
@@ -3,6 +3,11 @@ from day3.day3part1 import read_input, build_grid
 
 
 def one_true_elf(rectangles):
+    """
+    Returns the ID of the ElfRectangle which does not overlap with any other ElfRectangle
+    :param rectangles: the rectangles to be checked
+    :return: the ID of the non-overlapping rectangle, or 0 if none exists
+    """
     all_coords = build_grid(rectangles)
     for rect in rectangles:
         is_overlap = False
