a81f813df5aaa64f9015ac705591feafcb249830
Emily <emilyb3333@gmail.com>
2018-12-03 14:02:41 +1000

    Day 2 Part 1

---
+++ day2/day2part1.py
@@ -0,0 +1,55 @@
+import os
+
+
+def read_input(filename):
+    frequencies = []
+    with open(filename, 'r') as file:
+        for line in file:
+            frequencies.append(line)
+
+    return frequencies
+
+
+def get_letter_frequencies(word: str) -> dict:
+    frequencies = {}
+    for char in word:
+        if char not in frequencies:
+            frequencies[char] = 1
+        else:
+            frequencies[char] += 1
+
+    return frequencies
+
+
+def exactly_x_occurences(word: str, x: int) -> bool:
+    frequencies = get_letter_frequencies(word)
+    for char in frequencies:
+        if frequencies[char] == x:
+            return True
+
+    return False
+
+
+def get_checksum(words):
+    exactly_two = 0
+    exactly_three = 0
+    for word in words:
+        if exactly_x_occurences(word, 2):
+            exactly_two += 1
+
+        if exactly_x_occurences(word, 3):
+            exactly_three += 1
+
+    checksum = exactly_two * exactly_three
+    return checksum
+
+
+def main():
+    input_file = os.path.join(os.getcwd(), "input")
+    words = read_input(input_file)
+    checksum = get_checksum(words)
+    print("The checksum for this list is " + str(checksum))
+
+
+if __name__ == "__main__":
+    main()
