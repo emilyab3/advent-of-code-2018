bef1cc2a69589ca3edf9e14c413d6f6c19d0e546
Emily <emilyb3333@gmail.com>
2018-12-04 17:10:57 +1000

    Cleaned up and commented Day 4

--- day4/day4part1.py
+++ day4/day4part1.py
@@ -1,16 +1,24 @@
 import os
 import datetime
 from parse import *
+from typing import Tuple, Dict
 
 
 FALLS_ASLEEP = -1
 WAKES_UP = 0
 
 
-def decode_time(encoded: str):
-    # [1518-08-21 00:39] wakes up
-    parsed = parse("[{year:d}-{month:d}-{day:d} {hour:d}:{minute:d}] {message}", encoded)
-    date_time = datetime.datetime(parsed["year"], parsed["month"], parsed["day"], parsed["hour"], parsed["minute"])
+def decode_time(encoded: str) -> Tuple:
+    """
+    Takes an encoded string of the format "[yyyy-mm-dd hh:mm] some_message" and decodes
+    it into a datetime object and the message
+    :param encoded: the string to be decoded
+    :return: a datetime object extracted from the string and its corresponding message
+    """
+    parsed = parse("[{year:d}-{month:d}-{day:d} {hour:d}:{minute:d}] {message}",
+                   encoded)
+    date_time = datetime.datetime(parsed["year"], parsed["month"], parsed["day"],
+                                  parsed["hour"], parsed["minute"])
 
     if parsed["message"] == "wakes up":
         detail = WAKES_UP
@@ -22,7 +30,13 @@ def decode_time(encoded: str):
     return date_time, detail
 
 
-def read_input(filename: str):
+def read_input(filename: str) -> Dict:
+    """
+    Reads input from the given file and returns a mapping from the datetimes in the
+    file to their corresponding messages
+    :param filename: the file to read
+    :return: the mapping from datetimes to messages
+    """
     shifts = {}
     with open(filename, 'r') as file:
         for line in file:
@@ -32,29 +46,38 @@ def read_input(filename: str):
     return shifts
 
 
-def main():
-    input_file = os.path.join(os.getcwd(), "input")
-    shifts = read_input(input_file)
+def get_guard_movements(shifts: Dict) -> Tuple[Dict, Dict[int, Dict]]:
+    """
+    Parses the given raw shifts to return useful mappings
+    :param shifts: the shifts to parse
+    :return: a tuple of two different dictionary representations of the data -
+        1. Guard ID mapped to total time that guard spent sleeping
+        2. Guard ID mapped to {minute : number of times sleeping that minute}
+    """
+    # to ensure that the shifts are in order
+    chronological = sorted(shifts.keys())
 
-    dates = shifts.keys()
-    chronological = sorted(dates)
     total_time = {}
     shifts_worked = {}
+
     current_guard = None
     sleep_time = None
     for date in chronological:
         detail = shifts[date]
 
         if detail == WAKES_UP:
+            # mark each minute in the range before waking up as being asleep
             for minute in range(sleep_time.minute, date.minute):
                 if minute not in shifts_worked[current_guard]:
                     shifts_worked[current_guard][minute] = 0
 
                 shifts_worked[current_guard][minute] += 1
                 total_time[current_guard] += 1
-        elif detail == FALLS_ASLEEP:
 
+        elif detail == FALLS_ASLEEP:
+            # record that the guard is sleeping
             sleep_time = date
+
         else:
             current_guard = detail
             if current_guard not in shifts_worked:
@@ -62,6 +85,15 @@ def main():
             if current_guard not in total_time:
                 total_time[current_guard] = 0
 
+    return total_time, shifts_worked
+
+
+def sleepiest_guard(total_time: Dict[int, int]) -> int:
+    """
+    Finds the guard who spent the most time sleeping in total
+    :param total_time: a mapping from guards to the time they spent sleeping
+    :return: the guard who slept the longest overall
+    """
     longest_guard = None
     current_best = 0
     for guard in total_time:
@@ -69,30 +101,37 @@ def main():
             current_best = total_time[guard]
             longest_guard = guard
 
+    return longest_guard
+
+
+def sleepiest_minute(guard: int, shifts_worked: Dict) -> int:
+    """
+    Determines which minute (between 00:00 and 00:23) the given guard slept for the
+    longest overall
+    :param guard: the guard to examine
+    :param shifts_worked: the record of shifts worked
+    :return: the minute which the given guard slept the most at
+    """
     best_minute = -1
     best_count = 0
-    entries = shifts_worked[longest_guard]
+    entries = shifts_worked[guard]
     for minute in entries:
         if entries[minute] > best_count:
             best_count = entries[minute]
             best_minute = minute
 
+    return best_minute
+
+
+def main():
+    input_file = os.path.join(os.getcwd(), "input")
+    shifts = read_input(input_file)
+
+    total_time, shifts_worked = get_guard_movements(shifts)
+    longest_guard = sleepiest_guard(total_time)
+    best_minute = sleepiest_minute(longest_guard, shifts_worked)
+
     result = longest_guard * best_minute
-
-    print("The ID multiplied by the minute is {}".format(result))
-
-    best_minute = -1
-    best_guard = 0
-    best_count = 0
-    for guard in shifts_worked:
-        entries = shifts_worked[guard]
-        for minute in entries:
-            if entries[minute] > best_count:
-                best_count = entries[minute]
-                best_minute = minute
-                best_guard = guard
-
-    result = best_guard * best_minute
 
     print("The ID multiplied by the minute is {}".format(result))
 
---
+++ day4/day4part2.py
@@ -0,0 +1,40 @@
+import os
+from day4.day4part1 import read_input, get_guard_movements
+
+
+def most_consistently_sleepy_guard(shifts_worked):
+    """
+    Determines which guard is the most frequently asleep at the same minute from those
+    given in the shifts record
+    :param shifts_worked: the record of the shifts
+    :return: the guard who slept the most frequently at a single given minute, along
+        with which minute this was
+    """
+    best_minute = -1
+    best_guard = 0
+    best_count = 0
+    for guard in shifts_worked:
+        entries = shifts_worked[guard]
+        for minute in entries:
+            if entries[minute] > best_count:
+                best_count = entries[minute]
+                best_minute = minute
+                best_guard = guard
+
+    return best_guard, best_minute
+
+
+def main():
+    input_file = os.path.join(os.getcwd(), "input")
+    shifts = read_input(input_file)
+
+    total_time, shifts_worked = get_guard_movements(shifts)
+    best_guard, best_minute = most_consistently_sleepy_guard(shifts_worked)
+
+    result = best_guard * best_minute
+
+    print("The ID multiplied by the minute is {}".format(result))
+
+
+if __name__ == '__main__':
+    main()
