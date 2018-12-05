3b89934589d66df94f45b0f24950ce31288b9926
Emily <emilyb3333@gmail.com>
2018-12-05 20:10:59 +1000

    Cleaned up and commented Day 5

--- day5/day5part1.py
+++ day5/day5part1.py
@@ -1,7 +1,10 @@
 import os
 
 
-def read_input(filename: str):
+def read_input(filename: str) -> str:
+    """
+    Reads a polymer from the given file and returns it as a string
+    """
     polymer = ""
     with open(filename, 'r') as file:
         for line in file:
@@ -11,31 +14,49 @@ def read_input(filename: str):
 
 
 def opposite_polarity(unit1: str, unit2: str):
+    """
+    Returns True if the given units have opposite polarities (i.e. are different
+    cases), False otherwise
+    """
     return (unit1.isupper() and unit2.islower()) or \
            (unit1.islower() and unit2.isupper())
 
 
 def same_type(unit1: str, unit2: str):
+    """
+    Returns True if the given two units have the same type (i.e. are the same
+    letter, regardless of case), False otherwise
+    """
     return unit1.lower() == unit2.lower()
 
 
 def will_react(unit1: str, unit2: str):
+    """
+    Returns True if the given two units will react with each other (i.e. have opposite
+    polarities and the same type), False otherwise
+    """
     return opposite_polarity(unit1, unit2) and same_type(unit1, unit2)
 
 
 def process_polymer(polymer: str):
+    """
+    Processes the given polymer, removing any reacting pairs which are present
+    """
     remaining = polymer
     current = 0
     while current < len(remaining) - 1:
         first = remaining[current]
         second = remaining[current + 1]
+
         if will_react(first, second):
+            # cuts the reacting units out of the polymer
             if current != 0:
                 remaining = remaining[0:current] + remaining[current + 2:]
                 current -= 1
             else:
                 remaining = remaining[current + 2:]
         else:
+            # no reaction, progresses along the polymer
             current += 1
 
     return remaining
--- day5/day5part2.py
+++ day5/day5part2.py
@@ -3,6 +3,10 @@ from day5.day5part1 import read_input, process_polymer
 
 
 def get_components(polymer: str):
+    """
+    Returns a list of all the distinct characters which make up the given polymer
+    (all lowercase)
+    """
     components = []
     for unit in polymer:
         unit = unit.lower()
@@ -12,10 +16,14 @@ def get_components(polymer: str):
     return components
 
 
-def main():
-    input_file = os.path.join(os.getcwd(), "input")
-    polymer = read_input(input_file)
+def experiment(polymer):
+    """
+    Determines the resulting lengths of polymers produced by removing all instances
+    (both upper and lowercase) of a single character, and then processing the resulting
+    polymer.
 
+    Returns a mapping from character removed to resulting length.
+    """
     components = get_components(polymer)
     lengths = {}
     for unit in components:
@@ -24,14 +32,33 @@ def main():
         length = len(process_polymer(check_polymer))
         lengths[unit] = length
 
+    return lengths
+
+
+def best_to_remove(polymer, lengths):
+    """
+    Determines which character in the given polymer, if removed, will result in
+    the shortest length after processing
+    """
     shortest = len(polymer)
-    best_to_remove = ""
+    best = ""
     for unit, length in lengths.items():
         if length < shortest:
             shortest = length
-            best_to_remove = unit
+            best = unit
 
-    print("The best type to remove is {0}, resulting in a polymer of length {1}".format(best_to_remove, shortest))
+    return best
+
+
+def main():
+    input_file = os.path.join(os.getcwd(), "input")
+    polymer = read_input(input_file)
+
+    lengths = experiment(polymer)
+    best = best_to_remove(polymer, lengths)
+
+    print("The best type to remove is {0}, resulting in a polymer of "
+          "length {1}".format(best, lengths[best]))
 
 
 if __name__ == '__main__':
