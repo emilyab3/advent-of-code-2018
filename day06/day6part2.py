b1749c6cd1962f3d63493913a41411549b768795
Emily <emilyb3333@gmail.com>
2018-12-11 12:56:24 +1000

    Cleaned up and commented Day 6

--- day06/day6part2.py
+++ day06/day6part2.py
@@ -9,10 +9,12 @@ def main():
     input_file = os.path.join(os.getcwd(), "input")
     coords, mins, maxes = read_input(input_file)
 
-    goldilocks_zone = 0
+    goldilocks_zone = 0  # within the allowable area
     for x in range(mins[0], maxes[0] + 1):
         for y in range(mins[1], maxes[1] + 1):
             total_distance = 0
+
+            # determines sum of the distances to all of the coordinates
             for coord in coords:
                 total_distance += manhattan((x, y), coord)
 
